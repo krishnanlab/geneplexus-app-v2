d44a70549d5e89f4534165f99a185705d59e8c35
ChristopherMancuso <chrismancuso1984@gmail.com>
2023-11-17 16:36:48 -0500

    added functions for ML

--- functions/convert_ids/main.py
+++ functions/convert_ids/main.py
@@ -7,8 +7,20 @@ import geneplexus
 @functions_framework.http
 def convert_ids(request):
     """HTTP Cloud Function.
-    re add info on args and returns here at some point
+    set function up for CORS headers so function can be called from js
     """
+    if request.method == "OPTIONS":
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
+        return ("", 204, headers)
+    
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
+        
     # set the gp object
     # here net type, features and gsc are never used for gene conversion
     gp = geneplexus.GenePlexus(net_type="BioGRID", features="Embedding", gsc="GO",file_loc="data")
@@ -29,10 +41,16 @@ def convert_ids(request):
     # if there is geneids perform the following
     gp._load_genes(input_genes)
     gp._convert_to_entrez()
-    # convert to strings for ease of display
-    display_genes = [str(item) for item in gp.convert_ids]
-    display_genes = "-".join(display_genes)
-    # right now only pull out the list of genes
-    # but really need a datframe, I just have no
-    # idea how to display on a web page
-    return f"{display_genes}"
+    # # convert to strings for ease of display
+    # display_genes = [str(item) for item in gp.convert_ids]
+    # display_genes = "-".join(display_genes)
+    # # right now only pull out the list of genes
+    # # but really need a datframe, I just have no
+    # # idea how to display on a web page
+    # return (f"{display_genes}", 200, headers)
+    json_out = {}
+    json_out["convert_ids"] = gp.convert_ids
+    json_out["table_summary"] = gp.table_summary[:-1]
+    json_out["input_count"] = gp.input_count
+    json_out["df_convert_out"] = gp.df_convert_out.drop("In STRING-EXP?", axis=1).to_json(orient="split")
+    return (json_out, 200, headers)
