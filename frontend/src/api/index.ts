b9ced3f72006e1a31f9ee7e76906659551d5578f
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-07-26 00:37:25 -0400

    add cache at request

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,77 +3,48 @@ import { sleep } from "@/util/misc";
 /** base api url */
 export const api = import.meta.env.VITE_API;
 
-/** primitive param type */
-type Param = string | number | boolean | undefined | null;
-/** collection of params */
-export type Params = Record<string, Param | Param[]>;
+/** request cache */
+const cache = new Map<string, Response>();
 
-/** generic fetch request wrapper */
-export const request = async <Response>(
-  /** request path */
-  path = "",
-  /**
-   * key/value object for url parameters
-   *
-   * e.g. { ids: [1,2,3], sort: "asc" } -> ?ids=1,2,3&sort=asc
-   */
-  params: Params = {},
+/** general request */
+export async function request<Response>(
+  /** request url */
+  url: string | URL,
+  /** url parameters */
+  params: Record<string, string | string[]> = {},
   /** fetch options */
   options: RequestInit = {},
   /** parse response mode */
   parse: "text" | "json" = "json",
-): Promise<Response> => {
+) {
   /** artificial delay for testing loading spinners */
   await sleep(0);
 
-  /** get string of url parameters */
-  const paramsObject = new URLSearchParams();
+  /** make url object */
+  url = new URL(url);
+  /** construct params */
   for (const [key, value] of Object.entries(params))
-    paramsObject.append(key, [value].flat().join(","));
-
-  /** assemble url to query */
-  const url = path + "?" + paramsObject.toString();
-
-  /** make request object */
+    for (const param of [value].flat()) url.searchParams.append(key, param);
+  /** construct request */
   const request = new Request(url, options);
-
-  console.debug(`📞 Request ${path}`, {
-    url,
-    params,
-    options,
-    request,
-  });
-
-  /* make new request */
-  const response = await fetch(request);
-
-  /** capture error for throwing later */
-  let error = "";
-
-  /** check response code */
-  if (!response.ok) error = `Response not OK`;
-
+  /** unique request id for caching */
+  const id = JSON.stringify(request, ["url", "method", "headers"]);
+  /** get response from cache */
+  const cached = cache.get(id);
+  /** log info */
+  const log = `(${cached ? "cached" : "new"}) ${url}`;
+  console.debug(`📞 Request ${log}`, { request, options });
+  /** make request */
+  const response = cached ?? (await fetch(request));
+  /** check status code */
+  if (!response.ok) throw Error("Response not OK");
   /** parse response */
-  let parsed: Response | undefined;
-  try {
-    parsed =
-      parse === "text"
-        ? await response.clone().text()
-        : await response.clone().json();
-  } catch (e) {
-    error = `Couldn't parse response as ${parse}`;
-  }
-
-  console.debug(`📣 Response ${path}`, {
-    url,
-    params,
-    options,
-    parsed,
-    response,
-  });
-
-  /** throw error after details have been logged */
-  if (error || parsed === undefined) throw Error(error);
-
-  return parsed;
-};
+  const parsed =
+    parse === "text"
+      ? await response.clone().text()
+      : await response.clone().json();
+  console.debug(`📣 Response ${log}`, { response, parsed });
+  /** set cache for next time */
+  cache.set(id, response);
+  return parsed as Response;
+}
