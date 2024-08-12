764f66654e65893bde84dd7d43cfa7689d856916
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-08-12 14:43:40 -0400

    tweak request util method

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -19,7 +19,6 @@ export async function request<Response>(
 ) {
   /** artificial delay for testing loading spinners */
   await sleep(0);
-
   /** make url object */
   url = new URL(url);
   /** construct params */
@@ -33,18 +32,27 @@ export async function request<Response>(
   const cached = cache.get(id);
   /** log info */
   const log = `(${cached ? "cached" : "new"}) ${url}`;
-  console.debug(`📞 Request ${log}`, { request, options });
+  console.debug(`📞 Request ${log}`, { params, options, request });
   /** make request */
   const response = cached ?? (await fetch(request));
+  /** capture error for throwing later */
+  let error = "";
   /** check status code */
-  if (!response.ok) throw Error("Response not OK");
+  if (!response.ok) error = "Response not OK";
   /** parse response */
-  const parsed =
-    parse === "text"
-      ? await response.clone().text()
-      : await response.clone().json();
+  let parsed: Response | undefined;
+  try {
+    parsed =
+      parse === "text"
+        ? await response.clone().text()
+        : await response.clone().json();
+  } catch (e) {
+    error = `Couldn't parse response as ${parse}`;
+  }
   console.debug(`📣 Response ${log}`, { response, parsed });
+  /** throw error after details have been logged */
+  if (error || undefined) throw Error(error);
   /** set cache for next time */
   cache.set(id, response);
-  return parsed as Response;
+  return parsed;
 }
