86ee3b9a20d2437c7d57085277759e522fd121c1
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-11-20 11:47:58 -0500

    upstream fixes from molevolvr

--- frontend/src/util/download.ts
+++ frontend/src/util/download.ts
@@ -1,42 +1,90 @@
 import { stringify } from "csv-stringify/browser/esm/sync";
 
-/** download blob as file */
-export const download = (
-  data: BlobPart,
-  filename: string | string[],
-  type: string,
+type Filename = string | string[];
+
+/** download url as file */
+const download = (
+  /** url to download */
+  url: string,
+  /** single filename string or filename "parts" */
+  filename: Filename,
+  /** extension, without dot */
   ext: string,
 ) => {
-  const blob = new Blob([data], { type });
-  const url = window.URL.createObjectURL(blob);
   const link = document.createElement("a");
   link.href = url;
   link.download =
     [filename]
       .flat()
+      /** join parts */
       .join("_")
-      .replace(/[^ A-Za-z0-9_-]/g, " ")
-      .replace(/\s+/g, "-")
-      .replace(new RegExp("." + ext), "") +
+      /** make path-safe */
+      .replace(/[^A-Za-z0-9 -]+/g, " ")
+      /** consolidate underscores */
+      .replace(/_+/g, "_")
+      /** consolidate dashes */
+      .replace(/-+/g, "-")
+      /** consolidate spaces */
+      .replace(/\s+/g, " ")
+      /** remove extension if already included */
+      .replace(new RegExp("." + ext + "$"), "")
+      .trim() +
     "." +
     ext;
   link.click();
   window.URL.revokeObjectURL(url);
 };
 
+/** make url from blob */
+export const getUrl = (
+  /** blob data to download */
+  data: BlobPart,
+  /** mime type */
+  type: string,
+) =>
+  typeof data === "string" && data.startsWith("data:")
+    ? data
+    : window.URL.createObjectURL(new Blob([data], { type }));
+
 /** download table data as csv */
-export const downloadCsv = (data: unknown[], filename: string | string[]) =>
-  download(stringify(data), filename, "text/csv;charset=utf-8", "csv");
+export const downloadCsv = (data: unknown[], filename: Filename) =>
+  download(
+    getUrl(stringify(data, { header: true }), "text/csv;charset=utf-8"),
+    filename,
+    "csv",
+  );
+
+/** download table data as tsv */
+export const downloadTsv = (data: unknown[], filename: Filename) =>
+  download(
+    getUrl(
+      stringify(data, { header: true, delimiter: "\t" }),
+      "text/tab-separated-values",
+    ),
+    filename,
+    "tsv",
+  );
 
 /** download data as json */
-export const downloadJson = (data: unknown, filename: string | string[]) =>
-  download(JSON.stringify(data), filename, "application/json", "json");
+export const downloadJson = (data: unknown, filename: Filename) =>
+  download(getUrl(JSON.stringify(data), "application/json"), filename, "json");
+
+/** download blob as png */
+export const downloadPng = (data: BlobPart, filename: Filename) =>
+  download(getUrl(data, "image/png"), filename, "png");
+
+/** download blob as jpg */
+export const downloadJpg = (data: BlobPart, filename: Filename) =>
+  download(getUrl(data, "image/jpeg"), filename, "jpg");
 
 /** download svg element source code */
 export const downloadSvg = (
+  /** root svg element */
   element: SVGSVGElement,
-  filename: string | string[],
+  filename: Filename,
+  /** html attributes to add to root svg element */
   addAttrs: Record<string, string> = {},
+  /** html attributes to remove from any element */
   removeAttrs: RegExp[] = [/^class$/, /^data-.*/, /^aria-.*/],
 ) => {
   /** make clone of node to work with and mutate */
@@ -56,5 +104,5 @@ export const downloadSvg = (
         if (name.match(removeAttr)) element.removeAttribute(name);
 
   /** download clone source as svg file */
-  download(clone.outerHTML, filename, "image/svg+xml", "svg");
+  download(getUrl(clone.outerHTML, "image/svg+xml"), filename, "svg");
 };
